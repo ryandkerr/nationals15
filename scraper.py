6cc31cde485640654c1bb8723dade7cb2065f06a
ryandkerr <ryankerr@college.harvard.edu>
2015-05-31 03:19:52 -0400

    creates array with all data

--- scraper.py
+++ scraper.py
@@ -7,11 +7,16 @@ import csv
 
 BASE_URL = "http://play.usaultimate.org/events/USA-Ultimate-D-I-College-Championships-2015/"
 
+# helper for making soup
+def make_soup(url):
+    html = urlopen(url).read()
+    s = BeautifulSoup(html, "lxml")
+    return s
+
 # takes url and returns array of http links to team pages
 # returns both men and women team links
 def get_team_links(section_url):
-    html = urlopen(section_url).read()
-    soup = BeautifulSoup(html, "lxml")
+    soup = make_soup(section_url)
     team_table = soup.find("div", "eventSponsorInfo").table.find_all("a")
 
     link_list = []
@@ -27,17 +32,26 @@ team_links = get_team_links(BASE_URL)
 master_array = []
 
 def get_rows(team_url):
-    html = urlopen(team_url).read()
-    soup = BeautifulSoup(html, "lxml")
+    soup = make_soup(team_url)
     profile = soup.find("div", "profile_info")
     team_name = profile.h4.get_text(strip=True)
     division = profile.find("dl", id="CT_Main_0_dlGenderDivision").dd.get_text(strip=True)
 
-    # team_table = soup.find("table", "global_table")
+    team_rows = soup.find("table", "global_table").find_all("tr")
 
-    print team_name, division
+    for i, row in enumerate(team_rows):
+        if i > 0:
+            r =[team_name, division]
+            for cell in row.find_all("td"):
+                r.append(cell.get_text(strip=True))
+            master_array.append(r)
 
-get_rows(team_links[3])
+    # print team_name, division
+
+for team in team_links:
+    get_rows(team)
+
+print master_array
 
 
 
