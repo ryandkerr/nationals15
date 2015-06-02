3e80a9c14153b63028d9a606897a6c6191e80b2e
ryandkerr <ryankerr@college.harvard.edu>
2015-06-01 22:26:34 -0400

    working on shiny app, troubles with tooltips and keys

--- server.R
+++ server.R
@@ -2,9 +2,10 @@ library(shiny)
 library(BH)
 library(ggvis)
 indoor <- read.csv("data/nationals15.csv")
-indoor$Assists_per_Game <- round(indoor$Assists / indoor$Games, digits = 1)
-indoor$Goals_per_Game <- round(indoor$Goals/indoor$Games, digits = 1)
-indoor <- replace(indoor, is.na(indoor), 0)
+indoor$Key <- 1:nrow(indoor)
+# indoor$Assists_per_Game <- round(indoor$Assists / indoor$Games, digits = 1)
+# indoor$Goals_per_Game <- round(indoor$Goals/indoor$Games, digits = 1)
+# indoor <- replace(indoor, is.na(indoor), 0)
 
 shinyServer(function(input, output) {
 
@@ -19,17 +20,23 @@ shinyServer(function(input, output) {
     goal_max <- input$goal_range[2]
     
     # min and max games played from slider
-    games_min <- input$game_range[1]
-    games_max <- input$game_range[2]
+    # games_min <- input$game_range[1]
+    # games_max <- input$game_range[2]
         
     # filtering data
     s <- indoor[indoor$Assists >= ast_min &
                         indoor$Assists <= ast_max &
                         indoor$Goals >= goal_min &
-                        indoor$Goals <= goal_max &
-                        indoor$Games >= games_min &
-                        indoor$Games <= games_max,]
+                        indoor$Goals <= goal_max,]
+                        # indoor$Games >= gLames_min &
+                        # indoor$Games <= games_max,]
     
+    if(input$radio[1] == "mens") {
+      s <- s[s$Division == "Men's",]
+    } else {
+      s <- s[s$Division == "Women's",]
+    }
+
     # filter by team
     if(input$team[1] != "All Players") {
       s <- s[s$Team == input$team[1],]
@@ -40,42 +47,42 @@ shinyServer(function(input, output) {
         
   # tooltip function
   player_tooltip <- function(x) {
-    if(is.null(x)) return(NULL)
-    if(is.null(x$ID)) return(NULL)    
+    # if(is.null(x)) return(NULL)
+    # if(is.null(x$ID)) return(NULL)    
     
     players <- isolate(selection())
-    selected_player <- players[players$ID == x$ID,]
-    if(input$radio[1] == "totals") {  
-      paste0("<b>", selected_player$Player, "</b><br>",
-             "<b>Team: </b>", selected_player$Team, "<br>",
-             "<b>Ast/Gm: </b>", selected_player$Assists_per_Game, "<br>",
-             "<b>Gol/Gm: </b>", selected_player$Goals_per_Game)
-    } else {
-      paste0("<b>", selected_player$Player, "</b><br>",
-             "<b>Team: </b>", selected_player$Team, "<br>",
-             "<b>Assists: </b>", selected_player$Assists, "<br>",
-             "<b>Goals: </b>", selected_player$Goals)
-    }
+    selected_player <- players[players$Key == x$Key,]
+    # paste0("<b>", selected_player$Player, "</b><br>",
+    #        "<b>Team: </b>", selected_player$Team, "<br>",
+    #        "<b>Ast/Gm: </b>", selected_player$Assists_per_Game, "<br>",
+    #        "<b>Gol/Gm: </b>", selected_player$Goals_per_Game)
+    head(players)
+    
+    paste0("<b>", selected_player$Player, "</b><br>",
+           "<b>Team: </b>", selected_player$Team, "<br>",
+           "<b>Assists: </b>", selected_player$Assists, "<br>",
+           "<b>Goals: </b>", selected_player$Goals)
   }
   
   # creating ggvis scatterplot
   scatter <- reactive({
     # scatterplot of total points/ast
-    if(input$radio[1] == "totals") {
-      selection %>%
-        ggvis(~Assists, ~Goals, key := ~ID, text:= ~Player) %>%
-        layer_text(angle := 20) %>%
-        add_tooltip(player_tooltip, "hover")
+    # if(input$radio[1] == "totals") {
+    # key := ~ID
+    selection %>%
+      ggvis(~Assists, ~Goals, text:= ~Player) %>%
+      layer_text(angle := 20) %>%
+      add_tooltip(player_tooltip, "hover")
     
     # scatterplot of per game stats
-    } else {
-      selection %>%
-        ggvis(~Assists_per_Game, ~Goals_per_Game, key := ~ID, text := ~Player) %>%
-        layer_text(angle := 20) %>%
-        add_axis("x", title = "Assists/Game") %>%
-        add_axis("y", title = "Goals/Game") %>%
-        add_tooltip(player_tooltip, "hover")
-    }
+    # } else {
+      # selection %>%
+      #   ggvis(~Assists_per_Game, ~Goals_per_Game, key := ~ID, text := ~Player) %>%
+      #   layer_text(angle := 20) %>%
+      #   add_axis("x", title = "Assists/Game") %>%
+      #   add_axis("y", title = "Goals/Game") %>%
+      #   add_tooltip(player_tooltip, "hover")
+    # }
   })
       
   scatter %>% bind_shiny("scatter")
