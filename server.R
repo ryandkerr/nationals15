eaad4ce14792188309e0200710a4abef9a42f857
ryandkerr <ryankerr@college.harvard.edu>
2015-06-03 08:50:24 -0400

    ubuntu changes to match windows

--- server.R
+++ server.R
@@ -74,19 +74,19 @@ shinyServer(function(input, output) {
     } else if(input$radio2[1] == "Circles" && input$radio3 == "Totals") {
       selection %>%
         ggvis(~Assists, ~Goals, key := ~ID) %>%
-        layer_points(stroke := "black", fill = ~Team, size := 75, size.hover := 200,
+        layer_points(fill = ~Team, size := 75, size.hover := 200,
                      fillOpacity := 0.45, fillOpacity.hover := 0.7,
                      strokeWidth := 0) %>%
-        add_axis("x", title = "Assists per Game") %>%
-        add_axis("y", title = "Goals per Game") %>%
-        add_tooltip(player_tooltip, "hover") %>%
-        hide_legend("fill")
+        add_tooltip(player_tooltip, "hover")
     
     } else if(input$radio2[1] == "Names" && input$radio3 == "Per Game") {
       selection %>%
         ggvis(~Assists_per_Game, ~Goals_per_Game, key := ~ID, text := ~Player) %>%
         layer_text(angle := 20) %>%
-        add_tooltip(player_tooltip, "hover")
+        add_axis("x", title = "Assists per Game") %>%
+        add_axis("y", title = "Goals per Game") %>%
+        add_tooltip(player_tooltip, "hover") %>%
+        hide_legend("fill")
     } else {
       selection %>%
         ggvis(~Assists_per_Game, ~Goals_per_Game, key := ~ID) %>%
